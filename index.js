375f5cf9e0580178ba8752d66f38bf7814791b77
jdbuysse <jdbuysse@gmail.com>
2020-06-24 07:54:46 -0600

    added delete butttons to tasks

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function createChallengeHeader(challenge) {
     const title = document.createElement('h2')
     title.setAttribute('challenge-id', challenge.id)
     title.textContent = challenge.name
-    addTaskButton(title, challenge)
+    addChallengeButton(title, challenge)
     challengeHeader.appendChild(title)
 }
 
-function addTaskButton(card, challenge) {
+function addChallengeButton(card, challenge) {
     const button = document.createElement('button')
     button.setAttribute('data-id', challenge.id)
     button.textContent = 'Add task'
@@ -40,51 +40,55 @@ function addTasks(card, challenge) {
     challenge.tasks.forEach( task => {
         let tasks = document.createElement('li')
         tasks.setAttribute('task-id', task.id)
+        tasks.setAttribute('data-draggable', 'item')
+        tasks.setAttribute('draggable', 'true')
         tasks.textContent = task.description
+        addTaskButton(tasks)
         card.appendChild(tasks)
     })
 }
 
-function makeDraggable(){
+function addTaskButton(task){
+    let button = document.createElement("button")
+    button.addEventListener("click", deleteTask)
+    button.innerText = 'X'
+    task.append(button)
+}
 
-	for(let items = document.querySelectorAll('[data-draggable="item"]'), 
-		len = items.length, 
-		i = 0; i < len; i ++)
-	{
-		items[i].setAttribute('draggable', 'true');
-	}
+function deleteTask(){
+    alert('delete')
+}
 
-	var item = null;
+function makeDraggable() {
 
-	document.addEventListener('dragstart', function(e)
-	{
-		item = e.target;
-		e.dataTransfer.setData('text', '');
-	}, false);
+    for (let items = document.querySelectorAll('[data-draggable="item"]'),
+        len = items.length,
+        i = 0; i < len; i++) {
+        items[i].setAttribute('draggable', 'true');
+    }
 
-	document.addEventListener('dragover', function(e)
-	{
-		if(item)
-		{
-			e.preventDefault();
-		}
-	
-	}, false);	
+    let item = null;
 
-	
-	document.addEventListener('drop', function(e)
-	{
-		if(e.target.getAttribute('data-draggable') == 'target')
-		{
-			e.target.appendChild(item);
-			
-			e.preventDefault();
-		}
-	}, false);
-	
-	document.addEventListener('dragend', function(e)
-	{
-		item = null;
-	
-	}, false);
+    document.addEventListener('dragstart', function (e) {
+        item = e.target;
+        e.dataTransfer.setData('text', '');
+    }, false);
+
+    document.addEventListener('dragover', function (e) {
+        if (item) {
+            e.preventDefault();
+        }
+    }, false);
+
+
+    document.addEventListener('drop', function (e) {
+        if (e.target.getAttribute('data-draggable') == 'target') {
+            e.target.appendChild(item);
+            e.preventDefault();
+        }
+    }, false);
+
+    document.addEventListener('dragend', function (e) {
+        item = null;
+    }, false);
 }	
