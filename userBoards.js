7d675e22075c2b9735fff16b2a014adad1eb24b1
jdbuysse <jdbuysse@gmail.com>
2020-07-07 17:35:17 -0600

    fetch for new task

--- userBoards.js
+++ userBoards.js
@@ -72,7 +72,7 @@ function saveBoard(){
     let subtasks = Array.from(subtaskList.children).map(item => {
         return item.innerText
     })
-    if (localStorage.getItem("selected_task") === 0) {
+    if (localStorage.getItem("selected_task") === "0") {
         saveNewTask(subtasks)
     }
     else {
@@ -86,7 +86,39 @@ function updateExistingTask(subtasks){
 }
 
 function saveNewTask(subtasks){
+    title = taskName
     console.log("saving this as new task", subtasks)
+    fetchNewTask(title)
+    //this log happens too quickly for the fetch. what to do?
+    console.log(localStorage.getItem("selected_task"))
+}
+
+function fetchNewTask(){
+    let myHeaders = new Headers();
+    myHeaders.append("Authorization", `Bearer ${localStorage.getItem("token")}`);
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    let urlencoded = new URLSearchParams();
+    urlencoded.append("description", "placeholder");
+    urlencoded.append("title", title);
+
+    let requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+    };
+
+    fetch("http://127.0.0.1:8001/tasks/", requestOptions)
+    .then(response => response.text())
+    .then(result => returnNewTaskID(result))
+    .catch(error => console.log('error', error));
+}
+
+function returnNewTaskID(result){
+    obj = JSON.parse(result)
+    localStorage.setItem("selected_task", obj.id)
+    console.log('new task ID',obj.id)
 }
 
 function renderTask(task) {
